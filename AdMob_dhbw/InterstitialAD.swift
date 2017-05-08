9af3ac81507c9b1dc756e4a7d401a304860b2fcd
swesfsa <marcostierle@gmail.com>
2017-05-08 15:01:55 +0200

    interstatialAD added

--- AdMob_dhbw/InterstitialAD.swift
+++ AdMob_dhbw/InterstitialAD.swift
@@ -7,7 +7,40 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class AndereAD: UIViewController {
+class ViewController: UIViewController, GADInterstitialDelegate {
     
+    var interstitial: GADInterstitial?
+    
+    private func createAndLoadInterstitial() -> GADInterstitial? {
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-2752059782429024/8899451793")
+        
+        guard let interstitial = interstitial else {
+            return nil
+        }
+        
+        let request = GADRequest()
+        // next line is only needed if executed in the simulator
+        request.testDevices = [ kGADSimulatorID ]
+        interstitial.load(request)
+        interstitial.delegate = self
+        
+        return interstitial
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        interstitial = createAndLoadInterstitial()
+    }
+    
+    func interstitialDidReceiveAd(_ ad: GADInterstitial) {
+        print("Interstitial loaded successfully")
+        ad.present(fromRootViewController: self)
+    }
+    
+    func interstitialDidFail(toPresentScreen ad: GADInterstitial) {
+        print("Failed to receive interstitial")
+    }
 }
