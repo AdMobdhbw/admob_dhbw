c0078121a784627bdad241a23a23895dd8b52b0e
Philipp <philipp-enke@gmx.de>
2017-05-08 13:55:23 +0200

    Setup PageViewController

---
+++ AdMob_dhbw/AndereAD.swift
@@ -0,0 +1,13 @@
+//
+//  AndereView.swift
+//  AdMob_dhbw
+//
+//  Created by Philipp Enke on 08.05.17.
+//  Copyright © 2017 Philipp Enke. All rights reserved.
+//
+
+import UIKit
+
+class AndereAD: UIViewController {
+    
+}
---
+++ AdMob_dhbw/PageViewController.swift
@@ -0,0 +1,86 @@
+//
+//  PageViewController.swift
+//  AdMob_dhbw
+//
+//  Created by Philipp Enke on 08.05.17.
+//  Copyright © 2017 Philipp Enke. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource
+{
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController(ofType: "Banner"),
+                self.newViewController(ofType: "Andere")]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.white
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    func newViewController(ofType type: String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "\(type)AD")
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }    
+        
+        let previousIndex = viewControllerIndex - 1
+
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+    
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+}
