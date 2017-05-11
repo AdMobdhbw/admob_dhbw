6e6f80f4ea52d5dc422c057aa3e566b2a101d154
Philipp <philipp-enke@gmx.de>
2017-05-11 18:04:25 +0200

    Temporary deleted key

--- AdMob_dhbw/AppDelegate.swift
+++ AdMob_dhbw/AppDelegate.swift
@@ -18,8 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GADMobileAds.configure(withApplicationID: "ca-app-pub-2752059782429024/8442660995")
-        FIRApp.configure()
         return true
     }
 
